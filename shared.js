d1e8d463951fe4d70be599a96045bd80bb8097de
Anders Fisher <atleastimtrying@gmail.com>
2014-03-23 11:03:34 +0000

    WOOOAAAAHH hokey cokey cokey!

--- shared.js
+++ shared.js
@@ -1,54 +1,31 @@
 window.siv = {};
 window.siv.APIS = function(){
-  var jsonp = function(url, callback){
-    $.ajax({
-      type: 'GET',
-      url: url,
-      async: false,
-      contentType: "application/json",
-      dataType: 'jsonp',
-      beforeSend: function(){
-        $('#thinking').show();
-      },
-      complete: function(){
-        $('#thinking').hide();
-      },
-      success: function(data){
-        callback(data);
-      },
-      error: function(error){
-        console.log(error);
-      }
+  var hokey_cokey = function(label_in, label_out){
+    $(siv).on(label_in, function(event, url){
+      $.ajax({
+        type: 'GET',
+        url: url,
+        async: false,
+        contentType: "application/json",
+        dataType: 'jsonp',
+        beforeSend: function(){
+          $('#thinking').show();
+        },
+        complete: function(){
+          $('#thinking').hide();
+        },
+        success: function(response){
+          $(siv).trigger('emit_' + label_out, { emitted: response });
+        },
+        error: function(error){
+          console.log(error);
+        }
+      });
     });
   };
-
-  var emit_from_url = function(name, url){
-    jsonp(url, function(response){
-      emit(name, response);
-    });
-  };
-
-  var emit = function(string, object){
-    $(siv).trigger('emit_' + string, { emitted: object });
-  };
-
-  $(siv).on('retrieve_categories', function(event, url){
-    emit_from_url('categories', url);
-  });
-
-  $(siv).on('retrieve_subcategories', function(event, url){
-    emit_from_url('subcategories', url);
-  });
-  
-  $(siv).on('retrieve_subcategory', function(event, url){
-    emit_from_url('subcategory', url);
-  });
-  
-  $(siv).on('retrieve_company', function(event, url){
-    emit_from_url('company', url);
-  });
-  
-  $(siv).on('search_companies', function(event, url){
-    emit_from_url('company', url);
-  });
+  hokey_cokey('retrieve_categories', 'categories');
+  hokey_cokey('retrieve_subcategories', 'subcategories');  
+  hokey_cokey('retrieve_subcategory', 'subcategory');  
+  hokey_cokey('retrieve_company', 'company');  
+  hokey_cokey('search_companies', 'company');
 };
